5323774aca699e7b83782ebe7190ec5e6fba92c1
Halil ÇAKAR <hcakar.1992@gmail.com>
2018-10-01 04:21:38 +0300

    Added Notification future

--- App.js
+++ App.js
@@ -1,29 +1,72 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, Button, AppState } from 'react-native';
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import PushController from './src/PushController';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import PushNotification from 'react-native-push-notification';
 
-type Props = {};
-export default class App extends Component<Props> {
+export default class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {};
+    this.handleAppStateChange = this.handleAppStateChange.bind(this);
+    this.sendNotification = this.sendNotification.bind(this);
+  };
+
+  componentDidMount() {
+    AppState.addEventListener('change', this.handleAppStateChange);
+  };
+
+  componentWillUnmount() {
+    AppState.removeEventListener('change', this.handleAppStateChange);
+  };
+
+  // This will notify the user in 3 seconds after sending the app to the 
+  // background (like after pressing the home button or switching apps)
+  handleAppStateChange(appState) {
+    console.log(appState);
+    if (appState === 'background') {
+      PushNotification.localNotificationSchedule({
+        color: "#DE5F43",
+        title: "AppState::background",
+        message: 'Since background::Scheduled::done!', // (required)
+        date: new Date(Date.now() + (3 * 1000)) // in 3 secs
+      });
+    } 
+    else if(appState === 'active') {
+      PushNotification.localNotification({
+        title: "componentDidMount",
+        message: 'componentDidMount::done!',
+        color: "#DE5F43"
+      });
+    }
+  };
+
+  sendNotification() {
+    PushNotification.localNotification({
+      color: "#DE5F43", // (optional) default: system default
+      id: '123',
+      title: "Button Notification", // (optional)
+      message: 'sendNotification::done!'
+
+    });
+
+    // Schedule a notification
+    PushNotification.localNotificationSchedule({
+      title: 'Button Scheduled Notification',
+      message: 'Button Scheduled delay notification message 5sec::done!', // (required)
+      color: "#DE5F43",
+      date: new Date(Date.now() + (5 * 1000)) // in 3 secs
+    });
+  };
+
   render() {
     return (
       <View style={styles.container}>
+        <PushController />
         <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <Button title='Press here for a notification' onPress={this.sendNotification} />
       </View>
     );
   }
@@ -40,10 +83,5 @@ const styles = StyleSheet.create({
     fontSize: 20,
     textAlign: 'center',
     margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  }
 });
